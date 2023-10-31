91556d6886869fdff0d79fffb7683ac48ed60ab2
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-10-31 18:51:14 -0300

    fix: change melty company to logo png

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,18 @@
+import Melty from "../assets/Melty.png";
+
 export const Footer = () => {
   return (
-    <div className='mt-5 border-t-2 p-4 lg:p-10'>
-      <h2 className='text-center font-light'>Designed and coded with ❤️ by  <a target='_blank' className='underline underline-offset-4 text-lime-400 ' href="https://www.linkedin.com/in/gonzalo-arrayaran/">Melty.</a></h2>
+    <div className="mt-5 border-t-2 p-4 lg:p-10">
+      <h2 className="text-center font-light flex justify-center items-center gap-3">
+        Designed and coded with ❤️ by{" "}
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/gonzalo-arrayaran/"
+          className="mt-2"
+        >
+          <img className="w-[60px]" src={Melty} alt="" />
+        </a>
+      </h2>
     </div>
-  )
-}
+  );
+};
