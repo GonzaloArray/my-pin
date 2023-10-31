02f9710a83f39b3dfaacd9154a7e34c7940bf9cc
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-10-31 17:30:41 -0300

    fix: footer name gonzalo to Malty

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className='mt-5 border-t-2 p-4 lg:p-10'>
-      <h2 className='text-center font-light'>Designed and coded with ❤️ by  <a target='_blank' className='underline underline-offset-4 text-lime-400 ' href="">Gonzalo.</a></h2>
+      <h2 className='text-center font-light'>Designed and coded with ❤️ by  <a target='_blank' className='underline underline-offset-4 text-lime-400 ' href="https://www.linkedin.com/in/gonzalo-arrayaran/">Melty.</a></h2>
     </div>
   )
 }
