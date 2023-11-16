19e60cc3552d99102af23c61901462abdcfb90ae
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-11-16 17:32:40 -0300

    feat: new link

--- src/home/page/Home.tsx
+++ src/home/page/Home.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { useProfileStore } from "../../store/authProfileStore";
 
 export const Home = () => {
+  const user = useProfileStore((state) => state.user);
+
   return (
-    <div>
-      <h2>Acá va todo el home</h2>
-      <button>xxx</button>
-      <button>xxx</button>
-      <button>xxx</button>
-      <button>xxx</button>
-      <button>xxx</button>
-      <button>xxx</button>
+    <div className="h-screen flex flex-col gap-10 justify-center items-center">
+      <h1 className="text-6xl underline underline-offset-4">Resume Porfolio</h1>
+      {user.name !== "" && (
+        <Link
+          className="bg-gray-500 px-10 py-4 rounded-xl hover:bg-gray-400 text-lg"
+          to={`/${user.uid}`}
+        >
+          Go to Resume
+        </Link>
+      )}
     </div>
-  )
-}
+  );
+};
